1706fbeff30ead15a837abcee0fccd6c8931ff4a
Mehmet Yurtar <mehmet.yurtar@cimri.com>
2021-02-21 15:03:44 +0300

    add tasks api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { getDataFromApi, addTaskToApi } from './data';
+
 class PomodoroApp {
   constructor(options) {
     let { tableTbodySelector, taskFormSelector } = options;
@@ -6,26 +8,17 @@ class PomodoroApp {
     this.$taskFormInput = this.$taskForm.querySelector('input');
   }
 
-  getTasks() {
-    const storedTasks = window.localStorage.getItem('tasks');
-    if (storedTasks) {
-      return JSON.parse(storedTasks);
-    }
-    return [];
-  }
-
   addTask(task) {
-    const currentTasks = this.getTasks();
-    const newTasks = [...currentTasks, task];
-    const stringTasks = JSON.stringify(newTasks);
-    window.localStorage.setItem('tasks', stringTasks);
+    addTaskToApi(task)
+      .then((data) => data.json())
+      .then((newTask) => {
+        this.addTaskToTable(newTask);
+      });
   }
 
   addTaskToTable(task, index) {
-    const currentTasks = this.getTasks();
     const $newTaskEl = document.createElement('tr');
-    const taskIndex = index ? index : currentTasks.length;
-    $newTaskEl.innerHTML = `<th scope="row">${taskIndex}</th><td>${task.title}</td>`;
+    $newTaskEl.innerHTML = `<th scope="row">${task.id}</th><td>${task.title}</td>`;
     this.$tableTbody.appendChild($newTaskEl);
     this.$taskFormInput.value = '';
   }
@@ -33,16 +26,16 @@ class PomodoroApp {
   handleAddTask() {
     this.$taskForm.addEventListener('submit', (e) => {
       e.preventDefault();
-      const task = { id: Math.random(), title: this.$taskFormInput.value };
+      const task = { title: this.$taskFormInput.value };
       this.addTask(task);
-      this.addTaskToTable(task);
     });
   }
 
   fillTasksTable() {
-    const currentTasks = this.getTasks();
-    currentTasks.forEach((task, index) => {
-      this.addTaskToTable(task, index + 1);
+    getDataFromApi().then((currentTasks) => {
+      currentTasks.forEach((task, index) => {
+        this.addTaskToTable(task, index + 1);
+      });
     });
   }
 
